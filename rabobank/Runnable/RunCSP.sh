2fc6f0bb4e55380c62f30fb1ab2ff3a8be26a966
Adit2404 <54592730+Adit2404@users.noreply.github.com>
2023-05-12 17:11:46 +0200

    Update RunCSP.sh

--- rabobank/Runnable/RunCSP.sh
+++ rabobank/Runnable/RunCSP.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 
-
-# This will compile all the java files in the folder
-
-#find . -name "*.java" -print | xargs javac
-
-
-# Navigate to the directory where the Java program is saved and Execute the main file
-
-#cd /Users/adityabhargava/eclipse-workspace/CSPRabobank/bin/com/rabobank/csp/main/
-
-
 # Run the Java program with arguments
-#java CustomerStatementProcessor '/Users/adityabhargava/eclipse-workspace/CSPRabobank/resources/records.xml'
-
-
 java -jar rabobank-csp-0.0.1-SNAPSHOT.jar "records.xml"
